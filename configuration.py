f04c20b4f9550b6479e47a1ac64183a0b9cd13c9
Edward Laird <elaird@gmail.com>
2013-08-16 10:49:35 +0200

    init

---
+++ configuration.py
@@ -0,0 +1,18 @@
+from supy.defaults import *
+
+
+def mainTree():
+    return ("/", "Delphes")
+
+
+def initializeROOT(r, cppFiles=[]):
+    r.gROOT.SetStyle("Plain")
+    r.gStyle.SetPalette(1)
+    r.TH1.SetDefaultSumw2(True)
+    r.gErrorIgnoreLevel = 2000
+    r.gROOT.SetBatch(True)
+    r.gSystem.Load("../Delphes-3.0.10/libDelphes.so")
+
+
+def useCachedFileLists():
+    return False
