e16b2415854f5366542fffcdb0e1fd78a77e88a9
Edward Laird <elaird@gmail.com>
2013-08-23 11:09:15 +0200

    use c3 func

--- samples.py
+++ samples.py
@@ -1,31 +1,30 @@
 import supy
 from units import pb, fb
 
-holder = supy.samples.SampleHolder()
-holder.add("tt_test", '["/tmp/elaird/ttbar140PU_Phase2.root"]', xs=500*pb)
-holder.add("tt.conf0_0_600", '["root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/upgrade/PhaseI/Configuration0/140PileUp/tt-4p-0-600-v1510_14TEV/tt-4p-0-600-v1510_14TEV_354724341_PhaseI_Conf0_140PileUp.root"]', xs=500*pb)
+# test
+test = supy.samples.SampleHolder()
+test.add("tt_test", '["/tmp/elaird/ttbar140PU_Phase2.root"]', xs=500*pb)
 
-conf3 = "root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/upgrade/PhaseII/Configuration3/140PileUp"
-holder.add("tt.conf3_0_600",
-           '["%s/tt-4p-0-600-v1510_14TEV/tt-4p-0-600-v1510_14TEV_102886267_PhaseII_Conf3_140PileUp.root"]' % conf3,
-           xs=530.89358*pb)
 
-holder.add("tt.conf3_600_1100",
-           '["%s/tt-4p-600-1100-v1510_14TEV/tt-4p-600-1100-v1510_14TEV_101308277_PhaseII_Conf3_140PileUp.root"]' % conf3,
-           xs=42.55351*pb)
+# conf 0
+conf0 = supy.samples.SampleHolder()
+conf0s = "root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/upgrade/PhaseI/Configuration0/140PileUp"
+conf0.add("tt.conf0_0_600", '[%s/tt-4p-0-600-v1510_14TEV/tt-4p-0-600-v1510_14TEV_354724341_PhaseI_Conf0_140PileUp.root"]' % conf0s,
+          xs=500*pb)
 
-holder.add("tt.conf3_1100_1700",
-           '["%s/tt-4p-1100-1700-v1510_14TEV/tt-4p-1100-1700-v1510_14TEV_102571082_PhaseII_Conf3_140PileUp.root"]' % conf3,
-           xs=4.48209*pb)
+# conf 3
+def c3(dir="", skip=[]):
+    stem = "root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/upgrade/PhaseII/Configuration3/140PileUp/"
+    eos = "/afs/cern.ch/project/eos/installation/0.2.31/bin/eos.select"
+    func = "utils.io.fileListFromEos"
+    return '%s("%s", eos="%s", itemsToSkip=%s)' % (func, stem+dir+"/", eos, str(skip))
 
-holder.add("tt.conf3_1700_2500",
-           '["%s/tt-4p-1700-2500-v1510_14TEV/tt-4p-1700-2500-v1510_14TEV_100102772_PhaseII_Conf3_140PileUp.root"]' % conf3,
-           xs=0.52795*pb)
 
-holder.add("tt.conf3_2500_100000",
-           '["%s/tt-4p-2500-100000-v1510_14TEV/tt-4p-2500-100000-v1510_14TEV_102665566_PhaseII_Conf3_140PileUp.root"]' % conf3,
-           xs=0.05449*pb)
+conf3 = supy.samples.SampleHolder()
+conf3.add("tt.c3_0_600",       c3("tt-4p-0-600-v1510_14TEV"),       xs=530.89358*pb)
+conf3.add("tt.c3_600_1100",    c3("tt-4p-600-1100-v1510_14TEV"),    xs=42.55351*pb)
+conf3.add("tt.c3_1100_1700",   c3("tt-4p-1100-1700-v1510_14TEV"),   xs=4.48209*pb)
+conf3.add("tt.c3_1700_2500",   c3("tt-4p-1700-2500-v1510_14TEV"),   xs=0.52795*pb)
+conf3.add("tt.c3_2500_100000", c3("tt-4p-2500-100000-v1510_14TEV"), xs=0.05449*pb)
 
-holder.add("hh_bbtt.conf3",
-           '[%s]' % ",".join(['\"%s/GluGluToHHToBBTT_14TeV/GluGluToHHToBBTT_14TeV_%d.root\"' % (conf3, i) for i in range(4)]),
-           xs=40.0*fb)
+conf3.add("hh_bbtt.c3", c3("GluGluToHHToBBTT_14TeV", skip=["GluGluToHHToBBTT_14TeV_10.root"]), xs=40.0*fb)
