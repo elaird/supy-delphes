3ba0491f1eb871c065c0b6c610649078a63f5e44
Edward Laird <elaird@gmail.com>
2013-09-11 10:28:40 +0200

    more cut-flow

--- analyses/reco.py
+++ analyses/reco.py
@@ -5,7 +5,7 @@ import steps
 from units import fb, mb, mtau
 import ROOT as r
 
-class reco2(supy.analysis):
+class reco(supy.analysis):
     def listOfSteps(self, pars):
         return [supy.steps.printer.progressPrinter(),
                 supy.steps.histos.value("HT", 50, 0.0, 2500.0),
@@ -16,12 +16,31 @@ class reco2(supy.analysis):
                 supy.steps.histos.multiplicity("tauTagged_Jets"),
                 supy.steps.filters.multiplicity("tauTagged_Jets", min=2, max=2),
                 supy.steps.histos.value("HT", 50, 0.0, 2500.0),
-
+                
                 #supy.steps.histos.mass("bTagged_Jets_mask1_SumP4",              50, 0.0, 250.0),
                 #supy.steps.histos.mass("JetsFixedMass_bTagged_SumP4",           50, 0.0, 250.0),
                 supy.steps.histos.mass("JetsFixedMass_bTagged_Corrected_SumP4", 50, 0.0, 250.0),
                 supy.steps.filters.mass("JetsFixedMass_bTagged_Corrected_SumP4", min=85.0, max=150.0),
                 supy.steps.histos.mass("JetsFixedMass_bTagged_Corrected_SumP4", 50, 0.0, 250.0),
+                
+                #supy.steps.histos.mass("tauTagged_Jets_SumP4",                    50, 0.0, 250.0),
+                #supy.steps.histos.mass("JetsFixedMass_tauTagged_SumP4",           50, 0.0, 250.0),
+                supy.steps.histos.mass("JetsFixedMass_tauTagged_Corrected_SumP4", 25, 0.0, 250.0),
+                supy.steps.filters.mass("JetsFixedMass_tauTagged_Corrected_SumP4", min=75.0, max=175.0),
+                supy.steps.histos.mass("JetsFixedMass_tauTagged_Corrected_SumP4", 50, 0.0, 250.0),
+                supy.steps.histos.multiplicity("tauParticles"),
+
+                supy.steps.histos.mass("bbtt_LvSumP4", 32, 0.0, 800.0),
+                supy.steps.filters.mass("bbtt_LvSumP4", min=350.0),
+                
+                supy.steps.histos.pt("JetsFixedMass_bTagged_Corrected_SumP4", 12, 0.0, 300.0),
+                supy.steps.filters.pt("JetsFixedMass_bTagged_Corrected_SumP4", min=100.0),
+                
+                supy.steps.histos.pt("JetsFixedMass_tauTagged_Corrected_SumP4", 12, 0.0, 300.0),
+                supy.steps.filters.pt("JetsFixedMass_tauTagged_Corrected_SumP4", min=100.0),
+
+                supy.steps.histos.multiplicity("tauParticles"),
+                supy.steps.histos.value("HT", 50, 0.0, 2500.0),
                 ]
 
 
@@ -30,15 +49,28 @@ class reco2(supy.analysis):
         listOfCalculables += supy.calculables.zeroArgs(calculables)
         listOfCalculables += [calculables.HT(),
                               calculables.rho(),
+                              calculables.Filtered(pids=[-15, 15], label="tau", status=[3]),
                               calculables.Filtered(label="", ptMin=10.0, absEtaMax=2.4, key="Jet"),
                               calculables.bTagged("Jets", mask=0x1),
                               calculables.tauTagged("Jets"),
 
+                              # mbb
                               calculables.SumP4("bTagged_Jets_mask1"),
                               calculables.JetsFixedMass("bTagged_Jets_mask1", m=mb, label="bTagged"),
                               calculables.SumP4("JetsFixedMass_bTagged"),
                               calculables.JetsFixedMass("bTagged_Jets_mask1", m=mb, correctPt=True, label="bTagged"),
                               calculables.SumP4("JetsFixedMass_bTagged_Corrected"),
+
+                              # mtt
+                              calculables.SumP4("tauTagged_Jets"),
+                              calculables.JetsFixedMass("tauTagged_Jets", m=mtau, label="tauTagged"),
+                              calculables.SumP4("JetsFixedMass_tauTagged"),
+                              calculables.JetsFixedMass("tauTagged_Jets", m=mtau, correctPt=True, label="tauTagged"),
+                              calculables.SumP4("JetsFixedMass_tauTagged_Corrected"),
+
+                              # mbbtt
+                              calculables.LvSumP4(label="bbtt", keys=["JetsFixedMass_bTagged_Corrected_SumP4",
+                                                                      "JetsFixedMass_tauTagged_Corrected_SumP4"]),
                               ]
         return listOfCalculables
 
@@ -54,21 +86,24 @@ class reco2(supy.analysis):
 
         n = 100000
         nSmall = 40000
-        el = 2000/fb
+        el = 20000/fb
         return (specify(names="tt_c4_0_6_skim",   weights=w, color=r.kBlue, nEventsMax=nSmall) +
                 specify(names="tt_c4_6_11_skim",  weights=w, color=r.kBlue, nEventsMax=nSmall) +
                 specify(names="tt_c4_11_17_skim", weights=w, color=r.kBlue, nEventsMax=nSmall) +
                 specify(names="tt_c4_17_25_skim", weights=w, color=r.kBlue, nEventsMax=nSmall) +
                 specify(names="tt_c4_25_1k_skim", weights=w, color=r.kBlue, nEventsMax=nSmall) +
 
-                #specify(names="tt_0_6",   weights=w, color=r.kBlack, nEventsMax=n) +
-                #specify(names="tt_6_11",  weights=w, color=r.kBlack, nEventsMax=n) +
-                #specify(names="tt_11_17", weights=w, color=r.kBlack, effectiveLumi=40/fb) +
-                #specify(names="tt_17_25", weights=w, color=r.kBlack, nEventsMax=n) + #effectiveLumi=20/fb) +
-                #specify(names="tt_25_1k", weights=w, color=r.kBlack, nEventsMax=n) + #effectiveLumi=200/fb) +
+                #specify(names="tt_0_6",   weights=w) + #, color=r.kBlack, nEventsMax=n) +
+                #specify(names="tt_6_11",  weights=w) + #, color=r.kBlack, nEventsMax=n) +
+                #specify(names="tt_11_17", weights=w) + #, color=r.kBlack, effectiveLumi=40/fb) +
+                #specify(names="tt_17_25", weights=w) + #, color=r.kBlack, nEventsMax=n) + #effectiveLumi=20/fb) +
+                #specify(names="tt_25_1k", weights=w) + #, color=r.kBlack, nEventsMax=n) + #effectiveLumi=200/fb) +
+                
+                #specify(names="hh_bbtt_c4_10", color=r.kBlack,  effectiveLumi=el) +
+                #specify(names="hh_bbtt_c4_20", color=r.kBlack,  effectiveLumi=el) +
 
-                specify(names="hh_bbtt_c4_10", color=r.kBlack,  effectiveLumi=el) +
-                specify(names="hh_bbtt_c4_20", color=r.kBlack,  effectiveLumi=el) +
+                #specify(names="hh_bbtt_c4_10_skim", color=r.kBlack,  effectiveLumi=el) +
+                specify(names="hh_bbtt_c4_20_skim", color=r.kBlack,  effectiveLumi=el) +
                 []
                 )
 
@@ -85,10 +120,20 @@ class reco2(supy.analysis):
         #org.mergeSamples(targetSpec=gopts("tt_11_17", r.kCyan),    sources=["tt_11_17.GenWeight"])
         #org.mergeSamples(targetSpec=gopts("tt_17_25", r.kMagenta), sources=["tt_17_25.GenWeight"])
         #org.mergeSamples(targetSpec=gopts("tt_25_1k", r.kOrange),  sources=["tt_25_1k.GenWeight"])
-        org.mergeSamples(targetSpec=gopts("tt_skim", r.kBlack), allWithPrefix="tt_")
 
-        org.mergeSamples(targetSpec=gopts("hh_bbtt_c4_10", r.kRed), sources=["hh_bbtt_c4_10"])
-        org.mergeSamples(targetSpec=gopts("hh_bbtt_c4_20", r.kBlue), sources=["hh_bbtt_c4_20"])
+        #org.mergeSamples(targetSpec=gopts("hh_bbtt_c4_10", r.kRed), sources=["hh_bbtt_c4_10"])
+        #org.mergeSamples(targetSpec=gopts("hh_bbtt", r.kRed), sources=["hh_bbtt_c4_20"])
+
+
+        org.mergeSamples(targetSpec=gopts("tt_0_6",   r.kBlue),    sources=["tt_c4_0_6_skim.GenWeight"])
+        org.mergeSamples(targetSpec=gopts("tt_6_11",  r.kGreen),   sources=["tt_c4_6_11_skim.GenWeight"])
+        org.mergeSamples(targetSpec=gopts("tt_11_17", r.kCyan),    sources=["tt_c4_11_17_skim.GenWeight"])
+        org.mergeSamples(targetSpec=gopts("tt_17_25", r.kMagenta), sources=["tt_c4_17_25_skim.GenWeight"])
+        org.mergeSamples(targetSpec=gopts("tt_25_1k", r.kOrange),  sources=["tt_c4_25_1k_skim.GenWeight"])
+
+        org.mergeSamples(targetSpec=gopts("hh_bb#tau#tau", r.kRed), sources=["hh_bbtt_c4_20_skim"])
+        
+        org.mergeSamples(targetSpec=gopts("tt", r.kBlack), allWithPrefix="tt_", keepSources=True)
 
         org.scale(3.e3/fb)
         #org.scale(toPdf=True)
@@ -106,15 +151,15 @@ class reco2(supy.analysis):
                      #fitFunc=self.profFit,
                      showStatBox=False,
                      latexYieldTable=False,
-                     #samplesForRatios=("hh_bb#tau#tau", "tt"),
+                     #samplesForRatios=("hh_bbtt", "tt"),
                      #sampleLabelsForRatios=("hh", "tt"),
-                     #foms=[{"value": lambda x, y: x/y,
-                     #       "uncRel": lambda x, y, xUnc, yUnc: math.sqrt((xUnc/x)**2 + (yUnc/y)**2),
-                     #       "label": lambda x, y:"%s/%s" % (x, y),
-                     #       },
+                     foms=[{"value": lambda x, y: x/y,
+                            "uncRel": lambda x, y, xUnc, yUnc: math.sqrt((xUnc/x)**2 + (yUnc/y)**2),
+                            "label": lambda x, y:"%s/%s" % (x, y),
+                            },
                      #      #{"value": lambda x,y: x/(y**0.5),
                      #      # "uncRel": lambda x, y, xUnc, yUnc: math.sqrt((xUnc/x)**2 + (yUnc/y/2.)**2),
                      #      # "label": lambda x,y: "%s/sqrt(%s)" % (x, y),
                      #      # },
-                     #      ],
+                           ],
                      ).plotAll()
